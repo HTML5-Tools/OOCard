1c96c5311c2a9e602d72ad0522780369fd8a4329
budou10 <M-budou10@outlook.jp>
2026-05-27 21:32:15 +0900

    カードの進行状況をlocalStorageに保存するようにした

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const card_prev_elem = document.querySelector("#card-prev");
 const card_num_elem = document.querySelector("#card-num");
 const gen_card_button = document.querySelector("#genCard");
 const save_settings_button = document.querySelector("#saveSettings");
+const filter_checked_button = document.querySelector("#filterChecked");
 const hide_info_checkbox = document.querySelector("#hideInfo");
 const card_info_elem = document.querySelector("#card-info");
 STC_btn.addEventListener("click", () => {
@@ -15,9 +16,10 @@ const cardPrev = document.querySelector("#card-prev");
 const cardNum = document.querySelector("#card-num");
 const card_omote_input = document.querySelector("#card_omote_input");
 const card_ura_input = document.querySelector("#card_ura_input");
+let isProgress = false;
 
 let cardIndex = 0;
-let cardAmount = 0;
+let cardNumber = 0;
 function loadData() {
     const noData = localStorage.getItem("noData");
     if (noData === null){
@@ -34,16 +36,45 @@ function loadData() {
         } else {
             hide_info_checkbox.checked = false;
         }
+        isProgress = localStorage.getItem("isProgress") === "true";
+        if (isProgress){
+            cardNumber = parseInt(localStorage.getItem("cardNumber")) || 0;
+            cardIndex = parseInt(localStorage.getItem("cardIndex")) || 0;
+            const savedCardArr = localStorage.getItem("cardArr");
+            if (savedCardArr) {
+                cardArr = JSON.parse(savedCardArr);
+                cardText.textContent = cardArr[cardIndex];
+                cardNum.textContent = `${cardIndex + 1} / ${cardNumber}`;
+                const prevCard = cardArr[cardIndex - 1];
+                cardPrev.textContent = cardIndex % 2 != 0 ? prevCard : "---";
+                if (hide_info_checkbox.checked){
+                    card_info_elem.setAttribute("hidden", "true");
+                } else {
+                    card_info_elem.removeAttribute("hidden");
+                }
+            } else {
+                isProgress = false;
+            }
+        } else {
+            return;
+        }
     }
 };
-loadData();
-function saveData() {
+function saveData(manual = false) {
+    console.log("Saving data...");
     localStorage.setItem("card_ura_input", card_ura_input.value);
     localStorage.setItem("card_omote_input", card_omote_input.value);
     localStorage.setItem("hide_info", hide_info_checkbox.checked);
-    window.alert("入力を保存しました");
+    localStorage.setItem("isProgress", isProgress);
+    localStorage.setItem("cardArr", JSON.stringify(cardArr));
+    localStorage.setItem("cardIndex", cardIndex);
+    localStorage.setItem("cardNumber", cardNumber);
+    if (manual) {
+        window.alert("入力を保存しました");
+    }
+    return;
 };
-save_settings_button.addEventListener("click", saveData);
+save_settings_button.addEventListener("click", () => saveData(true));
 let cardArr = [];
 function shuffleCard() {
     let omoteArr = card_omote_input.value.split("\n");
@@ -76,21 +107,26 @@ function genCard() {
     cardPrev.textContent = "---";
     cardText.textContent = cardArr[0];
     cardIndex = 0;
-    cardAmount = cardArr.length;
-    cardNum.textContent = `${cardIndex + 1} / ${cardAmount}`;
+    cardNumber = cardArr.length;
+    cardNum.textContent = `${cardIndex + 1} / ${cardNumber}`;
     cardNum.removeAttribute("textcolor");
     if (hide_info_checkbox.checked){
         card_info_elem.setAttribute("hidden", "true");
     } else {
         card_info_elem.removeAttribute("hidden");
     }
+    isProgress = true;
+    saveData(false);
 }
 gen_card_button.addEventListener("click", genCard);
 function cardClicked() {
     cardIndex++;
-    if (cardIndex > cardAmount - 1){
+    saveData(false);
+    if (cardIndex > cardNumber - 1){
         cardNum.setAttribute("textcolor", "red");
         window.alert("カード帳の最後尾に到達しました。");
+        isProgress = false;
+        saveData(false);
     } else {    
         if (cardIndex % 2 === 0) {
             cardPrev.textContent = "---";
@@ -98,7 +134,7 @@ function cardClicked() {
             cardPrev.textContent = cardText.textContent;
         }
         cardText.textContent = cardArr[cardIndex];
-        cardNum.textContent = `${cardIndex + 1} / ${cardAmount}`;
+        cardNum.textContent = `${cardIndex + 1} / ${cardNumber}`;
     }
 }
 card_elem.addEventListener("click", cardClicked);
@@ -107,3 +143,7 @@ const title_elem = document.querySelector("#title");
 title_elem.addEventListener("click", () =>{
     location.href = "https://html5tools.netlify.app";
 });
+
+
+// 初期化
+loadData();
