de3b67581ccb2ff130b6ecd2ebb10801b594bfbc
budou10 <M-budou10@outlook.jp>
2026-05-31 11:40:03 +0900

    - 表、裏で背景色を変更するようにした - バグ解消

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const card_elem = document.querySelector("#card");
 const card_text_elem = document.querySelector("#card-text");
 const card_prev_elem = document.querySelector("#card-prev");
 const card_num_elem = document.querySelector("#card-num");
+const check_button = document.querySelector("#check_button");
 const gen_card_button = document.querySelector("#genCard");
 const save_settings_button = document.querySelector("#saveSettings");
 const filter_checked_button = document.querySelector("#filterChecked");
@@ -20,6 +21,11 @@ let isProgress = false;
 
 let cardIndex = 0;
 let cardNumber = 0;
+let cardArr = [];
+
+let newOmoteCardArr = []; 
+let newUraCardArr = [];
+
 function loadData() {
     const noData = localStorage.getItem("noData");
     if (noData === null){
@@ -41,12 +47,29 @@ function loadData() {
             cardNumber = parseInt(localStorage.getItem("cardNumber")) || 0;
             cardIndex = parseInt(localStorage.getItem("cardIndex")) || 0;
             const savedCardArr = localStorage.getItem("cardArr");
+            const savedNewOmoteCardArr = localStorage.getItem("newOmoteCardArr");
+            const savedNewUraCardArr = localStorage.getItem("newUraCardArr");
             if (savedCardArr) {
                 cardArr = JSON.parse(savedCardArr);
+                newOmoteCardArr = savedNewOmoteCardArr ? JSON.parse(savedNewOmoteCardArr) : [];
+                newUraCardArr = savedNewUraCardArr ? JSON.parse(savedNewUraCardArr) : [];
                 cardText.textContent = cardArr[cardIndex];
                 cardNum.textContent = `${cardIndex + 1} / ${cardNumber}`;
                 const prevCard = cardArr[cardIndex - 1];
                 cardPrev.textContent = cardIndex % 2 != 0 ? prevCard : "---";
+                card_elem.setAttribute("state", cardIndex % 2 === 0 ? "omote" : "ura");
+                const isChecked = localStorage.getItem("isChecked");
+                if (cardIndex % 2 != 0){
+                    if (isChecked === "true"){
+                        check_button.setAttribute("color", "green");
+                        check_button.removeAttribute("hidden");
+                    } else {
+                        check_button.setAttribute("color", "gray");
+                        check_button.removeAttribute("hidden");
+                    }
+                } else {
+                    check_button.setAttribute("hidden", "true");
+                }
                 if (hide_info_checkbox.checked){
                     card_info_elem.setAttribute("hidden", "true");
                 } else {
@@ -54,6 +77,9 @@ function loadData() {
                 }
             } else {
                 isProgress = false;
+            }
+            if (isProgress){
+                filter_checked_button.setAttribute("disabled", "true");
             }
         } else {
             return;
@@ -69,13 +95,15 @@ function saveData(manual = false) {
     localStorage.setItem("cardArr", JSON.stringify(cardArr));
     localStorage.setItem("cardIndex", cardIndex);
     localStorage.setItem("cardNumber", cardNumber);
+    localStorage.setItem("newOmoteCardArr", JSON.stringify(newOmoteCardArr));
+    localStorage.setItem("newUraCardArr", JSON.stringify(newUraCardArr));
+    localStorage.setItem("isChecked", check_button.getAttribute("color") === "green");
     if (manual) {
         window.alert("入力を保存しました");
     }
     return;
 };
 save_settings_button.addEventListener("click", () => saveData(true));
-let cardArr = [];
 function shuffleCard() {
     let omoteArr = card_omote_input.value.split("\n");
     let uraArr = card_ura_input.value.split("\n");
@@ -103,6 +131,8 @@ function genCard() {
         return;
     }
     cardArr = [];
+    newOmoteCardArr = [];
+    newUraCardArr = [];
     shuffleCard();
     cardPrev.textContent = "---";
     cardText.textContent = cardArr[0];
@@ -117,6 +147,10 @@ function genCard() {
     }
     isProgress = true;
     saveData(false);
+    filter_checked_button.setAttribute("disabled", "true");
+    check_button.setAttribute("color", "gray");
+    check_button.setAttribute("hidden", "true");
+    card_elem.setAttribute("state", "omote");
 }
 gen_card_button.addEventListener("click", genCard);
 function cardClicked() {
@@ -125,19 +159,58 @@ function cardClicked() {
     if (cardIndex > cardNumber - 1){
         cardNum.setAttribute("textcolor", "red");
         window.alert("カード帳の最後尾に到達しました。");
+        check_button.setAttribute("hidden", "true");
         isProgress = false;
         saveData(false);
+        filter_checked_button.removeAttribute("disabled");
     } else {    
         if (cardIndex % 2 === 0) {
             cardPrev.textContent = "---";
+            check_button.setAttribute("hidden", "true");
+            check_button.setAttribute("color", "gray");
+            card_elem.setAttribute("state", "omote");
         } else {
             cardPrev.textContent = cardText.textContent;
+            check_button.setAttribute("color", "gray");
+            check_button.removeAttribute("hidden");
+            newOmoteCardArr.push(cardArr[cardIndex - 1]);
+            newUraCardArr.push(cardArr[cardIndex]);
+            card_elem.setAttribute("state", "ura");
         }
         cardText.textContent = cardArr[cardIndex];
         cardNum.textContent = `${cardIndex + 1} / ${cardNumber}`;
     }
 }
-card_elem.addEventListener("click", cardClicked);
+function checkClicked() {
+    if (check_button.getAttribute("color") === "gray"){
+        check_button.setAttribute("color", "green");
+        localStorage.setItem("isChecked", true);
+        const index = newOmoteCardArr.indexOf(cardArr[cardIndex - 1]);
+        if (index !== -1) {
+            newOmoteCardArr.pop();
+            newUraCardArr.pop();
+        }
+    } else if (check_button.getAttribute("color") === "green"){
+        check_button.setAttribute("color", "gray");
+        localStorage.setItem("isChecked", false);
+        newOmoteCardArr.push(cardArr[cardIndex - 1]);
+        newUraCardArr.push(cardArr[cardIndex]);
+    }
+}
+function filterChecked() {
+    if (newOmoteCardArr.length === 0){
+        window.alert("カードがすべて除外されました！");
+    }
+    card_omote_input.value = newOmoteCardArr.join("\n");
+    card_ura_input.value = newUraCardArr.join("\n");
+}
+check_button.addEventListener("click", checkClicked);
+card_elem.addEventListener("click", () => {
+    if (!document.querySelector("#check_button:hover")) {
+        cardClicked();
+    }
+});
+filter_checked_button.addEventListener("click", filterChecked);
 // タイトルクリック処理
 const title_elem = document.querySelector("#title");
 title_elem.addEventListener("click", () =>{
